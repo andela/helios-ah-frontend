6e14705b58d28ee60a284c069cf49c6d6869ce38
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2019-01-09 22:26:29 +0100

    Implement feedback

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class App extends Component {
- render() {
-   return (
-     <div>
-       {this.props.children}
-       <h3 className="test2">Authors Haven</h3>
-     </div>
-   );
- }
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        <h3 className="test2">Authors Haven</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
