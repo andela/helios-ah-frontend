77684c0e00cbe77138693265437e88b852d32526
Chiagoziem Nwadike <chiagoziemnwadike@Chiagoziems-MacBook-Pro.local>
2019-01-10 11:56:39 +0100

    [Chore #161779732] Fix test decription.

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import App from '../../components/App';
 
 describe('Test to successfully render <App />', () => {
-  it('renders three <App /> components', () => {
+  it('renders the <App /> components', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.length).toBe(1);
   });
