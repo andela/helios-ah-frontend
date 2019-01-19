a10f3a2bd3266679eafe99261667df78f0e72cf0
Ademola Hussain <demola.hussainin@gmail.com>
2019-01-19 09:03:57 +0100

    [Chore #163271060] Resolve merge conflict

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -1,6 +1,11 @@
 import { Post } from '../utilities/apiRequests';
+import { USER_SIGNUP_SUCCESS } from '../actionTypes';
 
-const signupUser = userDetails => () => Post('/signup', userDetails)
+export const signupSuccess = userInfo => ({
+  type: USER_SIGNUP_SUCCESS,
+  userInfo,
+});
+export const signupUser = userDetails => () => Post('/auth/signup', userDetails)
   .then(response => response).catch((error) => {
     if (error.message === 'Network Error') {
       return {
@@ -10,5 +15,3 @@ const signupUser = userDetails => () => Post('/signup', userDetails)
     }
     return error;
   });
-
-export default signupUser;
--- src/components/CompleteRegistration.jsx
+++ src/components/CompleteRegistration.jsx
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Get } from '../utilities/apiRequests';
+import { signupSuccess } from '../actions/signupActions';
 
-const CompleteRegistration = ({ location }) => {
-  console.log('hello ==> ', location.search);
-  const baseUrl = `/auth/complete_reg${location.search}`;
-  console.log('base url is ==> ', baseUrl);
-  Get(baseUrl).then(res => console.log('res is ==> ', res))
-    .catch(err => console.log('err is ==> ', err));
-  return (
-    <div>
-      <h1>Yeah!</h1>
-    </div>
-  );
-};
+class CompleteRegistrationPage extends Component {
+  state = {
+    isLoggedIn: false,
+    pageStatus: 'Redirecting... ',
+  };
 
-CompleteRegistration.propTypes = {
+  componentDidMount() {
+    const { history, currentUser } = this.props;
+    if (currentUser.isAuthenticated) {
+      return history.push('/');
+    }
+  }
+
+  CompleteRegistration = () => {
+    const { location, history, signupUserSuccess } = this.props;
+    const baseUrl = `/auth/complete_reg${location.search}`;
+    Get(baseUrl).then((userInfo) => {
+      if (userInfo.success) {
+        localStorage.setItem('token', userInfo.token);
+        signupUserSuccess(userInfo);
+        this.setState({ isLoggedIn: true });
+      }
+    })
+      .catch((error) => {
+        console.log('err is ==> ', error);
+        this.setState({ pageStatus: 'Could not authenticate ' });
+        setTimeout(() => history.push('/signup'), 3000);
+      });
+  };
+
+  render() {
+    const { isLoggedIn, pageStatus } = this.state;
+    const { history } = this.props;
+    if (isLoggedIn) {
+      return history.push('/');
+    }
+    this.CompleteRegistration();
+    return (
+      <div>
+        <h1>{pageStatus}</h1>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  currentUser: state.currentUser,
+});
+
+const mapDispatchToProps = dispatch => ({
+  signupUserSuccess: userInfo => dispatch(signupSuccess(userInfo)),
+});
+
+CompleteRegistrationPage.propTypes = {
   location: PropTypes.objectOf(PropTypes.any).isRequired,
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+  signupUserSuccess: PropTypes.func.isRequired,
+  currentUser: PropTypes.objectOf(PropTypes.any).isRequired,
 };
-export default CompleteRegistration;
+
+export default
+connect(mapStateToProps, mapDispatchToProps)(CompleteRegistrationPage);
