e998b7a253b4aa4486e3b4d355d7844647ec9a3d
Ademola Hussain <demola.hussainin@gmail.com>
2019-01-21 19:50:53 +0100

    [Feature #163369071] Create card component

---
+++ src/components/card.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+// import cardImage from '../styles/images/DeadpoolSilence.jpg';
+
+const Card = () => (
+  <div className="row">
+    <div className="col-lg-4">
+      <div className="card w-30 text-center">
+        <img
+          className="card-img-top"
+          src="https://images.unsplash.com/photo-1547894233-3b986939e29c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="card"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Article title</h5>
+          <p className="card-text">
+            This is a wider card with supporting
+            text below as a natural lead-in to additional content.
+            This content is a little bit longer.
+
+          </p>
+          <p className="card-text">
+            <small className="text-muted">
+              Last updated 3 mins ago
+            </small>
+
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="col-lg-4">
+      <div className="card w-30 text-center">
+        <img
+          className="card-img-top"
+          src="https://images.unsplash.com/photo-1547894233-3b986939e29c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="card"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Article title</h5>
+          <p className="card-text">
+            This is a wider card with supporting
+            text below as a natural lead-in to additional content.
+            This content is a little bit longer.
+
+          </p>
+          <p className="card-text">
+            <small className="text-muted">
+              Last updated 3 mins ago
+            </small>
+
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="col-lg-4">
+      <div className="card w-30 text-center">
+        <img
+          className="card-img-top"
+          src="https://images.unsplash.com/photo-1547894233-3b986939e29c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          alt="card"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Article title</h5>
+          <p className="card-text">
+            This is a wider card with supporting
+            text below as a natural lead-in to additional content.
+            This content is a little bit longer.
+
+          </p>
+          <p className="card-text">
+            <small className="text-muted">
+              Last updated 3 mins ago
+            </small>
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+Card.propTypes = {};
+
+export default Card;
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Links from '../components/Links';
+import { Card } from '../components';
 
 const HomePage = () => (
   <div>
     <Links />
     <h2> This is the Home Page</h2>
+    <Card />
   </div>
 );
 
