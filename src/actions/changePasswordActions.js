5ef93b8b87a608bfcd1988a61aeb923afe124aa6
jideajayi11 <jideajayi11@gmail.com>
2019-01-25 18:57:37 +0100

    [Feature #163369071] fix change password validation

--- src/actions/changePasswordActions.js
+++ src/actions/changePasswordActions.js
@@ -1,4 +1,10 @@
 import { Put } from '../utilities/apiRequests';
+import { USER_SIGNUP_SUCCESS } from '../actionTypes';
+
+export const setCurrentUser = user => ({
+  type: USER_SIGNUP_SUCCESS,
+  user
+});
 
 const changePassword = passwordDetails => async () => {
   try {
--- src/components/ChangePassword.jsx
+++ src/components/ChangePassword.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import SubmitButton from './SubmitButton';
-import FormInput from './FormInput';
+import TextFieldGroup from './TextFieldGroup';
+import FlashMessageList from './FlashMessagesList';
 
 const ChangePassword = ({
   onChange,
+  errors,
+  onBlur,
+  onInput,
   password,
   isRequestSent,
   confirmPassword,
@@ -13,6 +17,7 @@ const ChangePassword = ({
 }) => (
   <div className="row v-flex-page">
     <div className="col-12" id="change-password-container">
+      <FlashMessageList customAlertClass="change-password-custom-alert col-12" />
       <div id="welcome-text-container" className="text-center">
         <h2>Change Password</h2>
         <br />
@@ -20,26 +25,40 @@ const ChangePassword = ({
       </div>
       <div id="change-password-form-container">
         <form className="form-horizontal">
-          <FormInput
+          <TextFieldGroup
             id="password"
+            error={errors && errors.password}
             value={password}
-            placeHolder="New Password"
+            placeholder="New Password"
+            field="password"
             onChange={onChange}
+            onBlur={onBlur}
+            errorFeedbackClass="signup-invalid-feedback"
+            validFeedbackClass="signup-valid-feedback"
+            customFormDivClass="signup-div-class"
+            onInput={onInput}
             type="password"
             className="form-control signup-form-input"
           />
-          <FormInput
+          <TextFieldGroup
             id="confirmPassword"
+            error={errors && errors.confirmPassword}
+            placeholder="Confirm Password"
+            field="confirmPassword"
             value={confirmPassword}
-            placeHolder="Confirm Password"
             onChange={onChange}
-            type="password"
+            onBlur={onBlur}
+            errorFeedbackClass="signup-invalid-feedback"
+            validFeedbackClass="signup-valid-feedback"
             className="form-control signup-form-input"
+            customFormDivClass="signup-div-class"
+            onInput={onInput}
+            type="password"
           />
           <SubmitButton
             onClick={submitDetails}
             isRequestSent={isRequestSent}
-            id="signup-page-button"
+            id="change-password-page-button"
             value="Submit"
             className="btn"
           />
@@ -55,10 +74,19 @@ const ChangePassword = ({
 
 ChangePassword.propTypes = {
   onChange: PropTypes.func.isRequired,
+  errors: PropTypes.oneOfType([
+    PropTypes.string, PropTypes.objectOf(PropTypes.string)]).isRequired,
   password: PropTypes.string.isRequired,
   confirmPassword: PropTypes.string.isRequired,
   submitDetails: PropTypes.func.isRequired,
   isRequestSent: PropTypes.bool.isRequired,
+  onInput: PropTypes.func,
+  onBlur: PropTypes.func
+};
+
+ChangePassword.defaultProps = {
+  onInput: () => null,
+  onBlur: () => null
 };
 
 export default ChangePassword;
