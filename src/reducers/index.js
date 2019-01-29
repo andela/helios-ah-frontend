629b4dc5840b8d81f8b04bdfa57f438331a6db60
jideajayi11 <jideajayi11@gmail.com>
2019-01-29 17:06:59 +0100

    [Feature #161779757] add bookmark list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import articles from './articleReducer';
 import flashMessages from './flashReducer';
 import authReducer from './authReducer';
 import homeReducer from './homeReducer';
+import bookmarkReducer from './bookmarkReducer';
 import {
   userReducer,
   buttonReducer,
@@ -17,6 +18,7 @@ const rootReducers = combineReducers({
   flashMessages,
   currentUser: authReducer,
   home: homeReducer,
+  bookmarks: bookmarkReducer,
 });
 
 export default rootReducers;
